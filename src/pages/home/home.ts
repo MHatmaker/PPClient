3ca5ce512aa96ee7be27e6293256183e7ef02f66
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2019-04-24 19:09:29 -0700

    works with no authentication

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AgoserviceProvider} from '../../providers/agoservice/agoservice';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private items : any;
+  private adrs : any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private itemsProvider : AgoserviceProvider) {
 
+  }
+  loadItems(){
+    console.log('loadItems called');
+    let items = this.itemsProvider.loadItems().subscribe(
+      data => {
+        let d : any = data;
+        this.items = d.results;
+      },
+      err => console.error(err),
+      // the third argument is a function which runs on completion
+      () => console.log('done loading items')
+    )
+  }
+  loadAddress() {
+    let address = this.itemsProvider.loadAddress().subscribe(
+      data => {
+        let d : any = data;
+        this.adrs = d.address.Address;
+      },
+      err => console.error(err),
+      // the third argument is a function which runs on completion
+      () => console.log('done loading address')
+    )
   }
 
 }
